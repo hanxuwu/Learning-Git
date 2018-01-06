d8d96bd0b5163bb9cacafd2ec412bc9aa598df54
hanxuwu <skunder301@gmail.com>
2018-01-06 14:02:49 +1100

    # test Iteration(#in master# Version 0.0.9 (solution 2))

--- TestIteration/README.md
+++ TestIteration/README.md
@@ -3,3 +3,7 @@
 ## Version 0.0.1 (only print)
 
 ## Version 0.0.2 (add loop)
+
+
+## Version 0.0.9 (solution 2)
+in master
--- TestIteration/TestIteration.py
+++ TestIteration/TestIteration.py
@@ -5,3 +5,7 @@ print('first version 0.0.1')
 print('second version 0.0.2')
 for i in range(5):
     print(i)
+
+# in master
+print("version 0.0.9")
+print('solution 2')
